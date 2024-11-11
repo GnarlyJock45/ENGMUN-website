620e416e34e24a0d2f9b5dd321684aab3eac2bf0
GnarlyJock45 <znrabaya@gmail.com>
2024-11-11 18:18:49 +0300

    added committee view

--- app.py
+++ app.py
@@ -19,5 +19,35 @@ def team():
 def contact():
     return render_template('contact.html', title='Contact Us')
 
+
+@app.route('/committee/<committee_id>')
+def committee(committee_id):
+    # Fetch committee data based on committee_id
+    committee_data = {
+        'name': 'DISEC',
+        'full_name': 'Disarmament and International Security',
+        'level': 'Beginner',
+        'delegates': 30,
+        'experience': 'Not Required',
+        'description': 'Detailed description...',
+        'topics': [
+            {
+                'title': 'Topic 1',
+                'description': 'Description of topic 1...'
+            },
+            # More topics...
+        ],
+        'resources': [
+            {
+                'title': 'Study Guide',
+                'description': 'Complete guide for delegates',
+                'url': '#'
+            },
+            # More resources...
+        ]
+    }
+    return render_template('committee.html', committee=committee_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
