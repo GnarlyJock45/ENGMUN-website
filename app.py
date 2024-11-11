fc2ba4a17161da6a2afecde2a332ada9d5f6e856
GnarlyJock45 <znrabaya@gmail.com>
2024-11-11 17:33:33 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/concert')
+def concert():
+    return render_template('concert.html', title='Concert Details')
+
+@app.route('/team')
+def team():
+    return render_template('team.html', title='Our Team')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
